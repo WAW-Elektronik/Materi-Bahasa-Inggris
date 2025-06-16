0109b0422cc072277c4a680404895f66353ed7a3
WAW-Elektronik <142165587+WAW-Elektronik@users.noreply.github.com>
2025-06-16 12:51:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+const content = {
+    tenses: `
+    <h2>Tenses</h2>
+<p><b>Tenses</b> adalah bentuk waktu dalam kalimat bahasa Inggris, dan sering membutuhkan <i>auxiliary verbs</i> untuk dibentuk secara tepat.</p>
+<div style="overflow-x:auto;">
+<table>
+  <tr>
+    <th>No</th>
+    <th>Tense</th>
+    <th>Struktur</th>
+    <th>Auxiliary Verb</th>
+    <th>Contoh</th>
+  </tr>
+  <tr><td>1</td><td>Simple Present</td><td>(+) Subject + V1 <br> (-) Subject + do/does + not + V1 <br> (?) Do/does + Subject + V1?</td><td>do/does</td><td>She plays football.<br>She does not play football.<br>Does she play football?</td></tr>
+  <tr><td>2</td><td>Present Continuous</td><td>(+) Subject + am/is/are + V-ing <br> (-) Subject + am/is/are + not + V-ing <br> (?) am/is/are + Subject + V-ing?</td><td>am, is, are</td><td>I am eating lunch.<br>I am not eating lunch.<br>Am I eating lunch?</td></tr>
+  <tr><td>3</td><td>Present Perfect</td><td>(+) Subject + has/have + V3 <br> (-) Subject + has/have + not + V3 <br> (?) has/have + Subject + V3?</td><td>has, have</td><td>He has finished the task.<br>He has not finished the task.<br>Has he finished the task?</td></tr>
+  <tr><td>4</td><td>Present Perfect Continuous</td><td>(+) Subject + has/have been + V-ing <br> (-) Subject + has/have not been + V-ing <br> (?) has/have + Subject + been + V-ing?</td><td>has been, have been</td><td>We have been studying.<br>We have not been studying.<br>Have we been studying?</td></tr>
+
+  <tr><td>5</td><td>Simple Past</td><td>(+) Subject + V2 <br> (-) Subject + did not + V1 <br> (?) Did + Subject + V1? </td><td>did</td><td>They visited Japan.<br>They did not visit Japan.<br>Did they visit Japan?</td></tr>
+  <tr><td>6</td><td>Past Continuous</td><td>(+) Subject + was/were + V-ing <br> (-) Subject + was/were + not + V-ing <br> (?) was/were + Subject + V-ing? </td><td>was, were</td><td>He was playing soccer.<br>He was not playing soccer.<br>Was he playing soccer?</td></tr>
+  <tr><td>7</td><td>Past Perfect</td><td>(+) Subject + had + V3 <br> (-) Subject + had + not + V3 <br> (?) had + Subject + V3? </td><td>had</td><td>She had left.<br>She had not left.<br>Had she left?</td></tr>
+  <tr><td>8</td><td>Past Perfect Continuous</td><td>(+) Subject + had been + V-ing <br> (-) Subject + had not been + V-ing <br> (?) had been + Subject + V-ing?</td><td>had been</td><td>I had been sleeping.<br>I had not been sleeping.<br>Had I been sleeping?</td></tr>
+
+  <tr><td>9</td><td>Simple Future</td><td>(+) Subject + will + V1 <br> (-) Subject + will + not + V1 <br> (?) will + Subject + V1?</td><td>will</td><td>I will read the book.<br>I will not read the book.<br>Will I read the book?</td></tr>
+  <tr><td>10</td><td>Future Continuous</td><td>(+) Subject + will be + V-ing <br> (-) Subject + will not be + V-ing <br> (?) will + Subject + be + V-ing?</td><td>will be</td><td>She will be working.<br>She will not be working.<br>Will she be working?</td></tr>
+  <tr><td>11</td><td>Future Perfect</td><td>(+) Subject + will have + V3 <br> (-) Subject + will + not + have + V3 <br> (?) will + Subject + have + V3?</td><td>will have</td><td>He will have eaten.<br>He will not have eaten.<br>Will he have eaten?</td></tr>
+  <tr><td>12</td><td>Future Perfect Continuous</td><td>(+) Subject + will have been + V-ing <br> (-) Subject + will + not + have been + V-ing + <br> (?) will + Subject + have been + V-ing?</td><td>will have been</td><td>They will have been learning.<br>They will not have been learning.<br>Will they have been learning?</td></tr>
+
+  <tr><td>13</td><td>Simple Past Future</td><td>(+) Subject + would + V1 <br> (-) Subject + would + not + V1 <br> (?) would + Subject + V1?</td><td>would</td><td>I would help her.<br>I would not help her.<br>Would I help her?</td></tr>
+  <tr><td>14</td><td>Past Future Continuous</td><td>(+) Subject + would be + V-ing <br> (-) Subject + would + not + be + V-ing <br> (?) would + Subject + be + V-ing?</td><td>would be</td><td>He would be running.<br>He would not be running.<br>Would he be running?</td></tr>
+  <tr><td>15</td><td>Past Future Perfect</td><td>(+) Subject + would have + V3 <br> (-) Subject + would not have + V3 <br> (?) would + Subject + have + V3?</td><td>would have</td><td>She would have arrived.<br>She would not have arrived.<br>Would she have arrived?</td></tr>
+  <tr><td>16</td><td>Past Future Perfect Continuous</td><td>(+) Subject + would have been + V-ing <br> (-) Subject + would not have been + V-ing <br> (?) would + Subject + have been + V-ing?</td><td>would have been</td><td>He would have been sleeping.<br>He would not have been sleeping.<br>Would he have been sleeping?</td></tr>
+</table>
+</div>
+    <br> Simple Present	Menyatakan fakta umum, kebiasaan, dan rutinitas.
+    <br> Present Continuous	Menyatakan aksi yang sedang berlangsung sekarang atau rencana dekat.
+    <br> Present Perfect Menunjukkan kejadian yang sudah terjadi namun masih relevan sekarang.
+    <br> Present Perfect Continuous	Menunjukkan aktivitas yang dimulai di masa lalu dan masih berlangsung.
+    <br> Simple Past Menggambarkan aksi yang selesai di masa lalu dengan waktu tertentu.
+    <br> Past Continuous Menyatakan aksi yang sedang berlangsung pada waktu tertentu di masa lalu, atau dua aksi paralel.
+    <br> Past Perfect Menjelaskan kejadian yang terjadi sebelum kejadian lain di masa lalu.
+    <br> Past Perfect Continuous Aksi berkelanjutan sebelum titik waktu tertentu di masa lalu.
+    <br> Simple Future	Menyatakan rencana spontan, janji, atau prediksi.
+    <br> Future Continuous	Aksi yang akan sedang terjadi di masa depan pada waktu tertentu.
+    <br> Future Perfect	Aksi yang akan selesai sebelum waktu atau aksi lain di masa depan.
+    <br> Future Perfect Continuous Menekankan durasi aksi sebelum titik waktu di masa depan.
+    <br> Simple Past Future	Prediksi atau rencana masa depan dari sudut pandang masa lalu.
+    <br> Past Future Continuous	Menunjukkan aksi yang diperkirakan akan sedang berlangsung dari masa lalu.
+    <br> Past Future Perfect Aksi yang diperkirakan sudah selesai dalam konteks masa lalu.
+    <br> Past Future Perfect Continuous	Menunjukkan durasi aksi yang diprediksi terjadi dan masih berlangsung dalam konteks masa lalu.
+
+`/*<p>Catatan Tambahan:
+     <br> Tenses seperti <b>Past Future</b> digunakan dalam reported speech, conditional sentences, atau narasi masa lalu yang merujuk ke masa depan.
+     <br> Tenses masa lampau seperti Past Perfect sering digunakan dalam reported speech:
+     <br> Tenses campuran seperti Past Future digunakan saat merujuk ke masa depan dalam cerita masa lalu, terutama dalam kalimat bersyarat (conditional sentences type 2 dan 3) atau narasi
+     </p>*/
+,
+  primary: `
+    <h2>Primary Auxiliary Verbs</h2>
+    <p>Digunakan untuk membentuk tense, bentuk pasif, atau kalimat negatif dan tanya.</p>
+    <ul>
+      <li><b>Be</b>: am, is, are, was, were, be, been, being</li>
+      <li><b>Have</b>: have, has, had</li>
+      <li><b>Do</b>: do, does, did</li>
+    </ul>
+    <p><b>Contoh Kalimat:</b></p>
+    <ul>
+      <li>She <b>is</b> reading a book. (Present Continuous)</li>
+      <li>They <b>have</b> finished their work. (Present Perfect)</li>
+      <li>He <b>did</b> not come. (Simple Past Negative)</li>
+    </ul>
+  `,
+
+  modal: `
+    <h2>Modal Auxiliary Verbs</h2>
+    <p>Digunakan untuk menunjukkan kemungkinan, kemampuan, izin, keharusan, dll.</p>
+    <table>
+        <tr><th>Modal</th><th>Makna</th><th>Contoh</th></tr>
+        <tr><td>can	</td><td>kemampuan, izin	</td><td>She can swim. / Can I come in?
+        <tr><td>could	</td><td>kemampuan lampau, kemungkinan	</td><td>He could run fast.
+        <tr><td>may	</td><td>kemungkinan, izin	</td><td>It may rain. / May I help you?
+        <tr><td>might	</td><td>kemungkinan kecil	</td><td>We might be late.
+        <tr><td>must	</td><td>keharusan kuat	</td><td>You must wear a helmet.
+        <tr><td>shall	</td><td>tawaran, masa depan	</td><td>Shall we begin?
+        <tr><td>should	</td><td>saran, keharusan	</td><td>You should see a doctor.
+        <tr><td>will	</td><td>masa depan, niat	</td><td>I will call you tomorrow.
+        <tr><td>would	</td><td>bentuk lampau dari will/conditional	</td><td>I would go if I could.
+    </table>
+  `,
+
+  semi: `
+    <h2>Semi-Auxiliary Verbs</h2>
+    <p>Frasa kerja bantu yang digunakan seperti auxiliary verbs.</p>
+    <table>
+        <tr><th>Frasa</th><th>Makna</th><th>Contoh</th></tr>
+        <tr><td>be going to	</td><td>akan (future plan/intent)	</td><td>I <b>am going to </b>start a new project.</td></tr>
+        <tr><td>have to	</td><td>harus (necessity/obligation)	</td><td>She <b>has to </b>study for the exam.</td></tr>
+        <tr><td>be able to	</td><td>bisa (ability)	</td><td>He <b>is able to </b>lift that box.</td></tr>
+        <tr><td>used to	</td><td>dulu biasa (past habit/state)	</td><td>We <b>used to </b>live in the city.</td></tr>
+        <tr><td>be about to	</td><td>akan segera (immediate future)	</td><td>The train <b>is about to </b>leave.</td></tr>
+        <tr><td>be supposed to	</td><td>seharusnya (expectation/duty)	</td><td>You <b>are supposed to </b>be in class.</td></tr>
+        <tr><td>be to	</td><td>harus (formal obligation/instruction)	</td><td>You <b>are to </b>report to the manager.</td></tr>
+        <tr><td>be likely to	</td><td>kemungkinan besar (probability)	</td><td>He <b>is likely to </b>win the race.</td></tr>
+        <tr><td>be willing to	</td><td>bersedia (readiness)	</td><td>She <b>is willing to </b>help us.</td></tr>
+        <tr><td>be allowed to	</td><td>diizinkan (permission)	</td><td>Are we <b>allowed to </b>bring phones?</td></tr>
+        <tr><td>be meant to	</td><td>dimaksudkan untuk (intended purpose)	</td><td>This tool <b>is meant to </b>cut wood.</td></tr>
+        <tr><td>be eager to	</td><td>sangat ingin (strong desire)	</td><td>They <b>are eager to </b>learn more.</td></tr>
+        <tr><td>be ready to	</td><td>siap untuk (readiness)	</td><td>We <b>are ready to </b>begin.</td></tr>
+        <tr><td>be permitted to	</td><td>diizinkan (formal permission)	</td><td>You <b>are permitted to </b>enter the lab.</td></tr>
+    </table>
+  `
+};
+
+function showContent(type) {
+  const area = document.getElementById('contentArea');
+  area.innerHTML = content[type];
+}
